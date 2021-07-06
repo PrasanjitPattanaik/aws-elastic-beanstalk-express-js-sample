7d6721fdeca7f816b1c6e2514f715b7e62fa3b33
Prasanjit Pattanaik <prasanjit.pattanaik@yandex.com>
2021-07-06 17:27:11 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Saaspect'));
+app.get('/', (req, res) => res.send('Hello Saaspect PipeLine is setup'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
